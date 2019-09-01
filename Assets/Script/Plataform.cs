2e96dd931a4591cd9533747f4a2c3186f2600e77
DanielGeraldino <danielguerra@ucl.br>
2019-09-01 13:33:24 -0300

    plataforma e cenario
    inserçao do script da platorfama que se move e desenhando o cenario

---
+++ Assets/Script/Plataform.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataform : MonoBehaviour
+{
+    public float velocidaMovimento = 1.0f;
+    private int cont;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cont <= 500)
+        {
+            MovePlataforma(1);
+        }
+        if (cont > 500 && cont <= 1000)
+        {
+            MovePlataforma(-1);
+        }
+        if (cont > 1000)
+        {
+            cont = 0;
+        }
+
+        cont++;
+    }
+
+    void MovePlataforma(int direcao)
+    {
+        rb.velocity = new Vector2(direcao * velocidaMovimento, rb.velocity.y);
+    }
+}
