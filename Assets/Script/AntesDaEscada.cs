6cce76811250d6bf43c6dfa9d261ab5686f4dc07
DanielGeraldino <danielguerra@ucl.br>
2019-09-19 22:54:37 -0300

    Inserido os texto de comando na UI e criado script para o personagem não flutuar na area antesEscada

---
+++ Assets/Script/AntesDaEscada.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntesDaEscada : MonoBehaviour
+{
+    public GameObject personagem;
+    private float forceMagnitudeInicio;
+
+    private void Start()
+    {
+        forceMagnitudeInicio = GetComponent<AreaEffector2D>().forceMagnitude;
+    }
+    void Update()
+    {
+        if (personagem.GetComponent<ControlePorVoz>().parar)
+        {
+            GetComponent<AreaEffector2D>().forceMagnitude = 0;
+        }
+        else
+        {
+            GetComponent<AreaEffector2D>().forceMagnitude = this.forceMagnitudeInicio;
+        }
+        
+    }
+}
