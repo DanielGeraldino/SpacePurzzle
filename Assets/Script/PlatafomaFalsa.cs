0ef0e6add924e5238e2eccb3c5993cc376a81c25
DanielGeraldino <danielguerra@ucl.br>
2019-09-28 22:35:48 -0300

    Finalizando a plataforma falsa
    Esta plataforma permite que o usuario entre de baixo para cima.

--- Assets/Script/PlatafomaFalsa.cs
+++ Assets/Script/PlatafomaFalsa.cs
@@ -5,24 +5,13 @@ using UnityEngine;
 public class PlatafomaFalsa : MonoBehaviour
 {
 
-    // TODO: cria uma forma do personagem entra por baixo na plataforma.
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject plataforma;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("player"))
         {
-            GetComponent<BoxCollider2D>().isTrigger = false;
+            plataforma.GetComponent<BoxCollider2D>().isTrigger = false;
         }
     }
 
