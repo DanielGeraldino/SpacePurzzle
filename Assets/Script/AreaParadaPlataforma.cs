091eac61db7de4d7955b61421e3caee8da3cdb68
DanielGeraldino <danielguerra@ucl.br>
2019-10-01 19:36:05 -0300

    Implementando um ponto de para  plataforma após ter coletado o primeiro cristal

---
+++ Assets/Script/AreaParadaPlataforma.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaParadaPlataforma : MonoBehaviour
+{
+    public GameObject plataforma;
+    public GameObject jogador;
+    private int qtdCristal;
+
+    private void Update()
+    {
+        qtdCristal = jogador.GetComponent<ControlePorVoz>().qtdCristal;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(qtdCristal >= 1 && collision.gameObject.CompareTag("player"))
+        {
+            plataforma.GetComponent<Plataforma>().enabled = false;
+        }
+    }
+}
